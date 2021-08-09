56724e3d6b13fab9f1082316c4ddab926bae2b50
Paulo <pauloh16.pdr@gmail.com>
2021-08-09 09:10:00 -0300

    Ajustar configuracao para site name

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://desolate-retreat-97406.herokuapp.com';
 export const POSTS_URL = `${API_URL}/posts`;
 
 export const SITE_NAME = 'Paulo Henrique';
-export const SITE_URL = process.env.SITE_URL || 'http://127.0.0.1:3000';
+export const SITE_URL = 'https://dazzling-wiles-489ab1.netlify.app/';
